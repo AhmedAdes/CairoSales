1414f54417e0b82ef95dd2b246d5083b5df99e7d
AhmedAdes <ahmed.eleccom@gmail.com>
2017-08-06 13:49:01 +0200

    change sql ip

--- SQLConfig.js
+++ SQLConfig.js
@@ -20,7 +20,7 @@
     IDIConfig: {
         user: 'sa',
         password: 'ITD@reY0u2',
-        server: '105.199.10.121,33333', //105.199.10.121,33333//105.198.224.91//SQL-SRV // You can use 'localhost\\instance' to connect to named instance  
+        server: '105.198.224.91,33333', //105.199.10.121,33333//105.198.224.91//SQL-SRV // You can use 'localhost\\instance' to connect to named instance  
         database: 'IDIData',
         options: {
             encrypt: true // Use this if you're on Windows Azure
