6b155b4fe0e1cef8275fc4168ef90f1b364b6e18
AhmedAdes <ahmed.eleccom@gmail.com>
2017-08-29 15:58:41 +0200

    change basicdata index.ts

--- public/src/app/components/BasicData/index.ts
+++ public/src/app/components/BasicData/index.ts
@@ -2,7 +2,7 @@ export * from './gifts/gift.component';
 export * from './drugs/drug.component';
 export * from './importance/importance.component';
 export * from './ims/ims.component';
-export * from './medspecs/medspec.component';
+export * from './medSpecs/medspec.component';
 export * from './promoTools/promo.component';
 export * from './saleslines/lines.component';
 export * from './basic.component';
